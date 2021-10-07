bc01a78142d22c5fe55ca5355e3284683011f452
rapha <raphael.valeri@ensta-bretagne.org>
2021-10-07 18:21:51 +0200

    correction module java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,9 +2,11 @@ module com.example.projetfrogger {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.desktop;
-
-    requires org.kordamp.bootstrapfx.core;
+    requires javafx.graphics;
 
     opens com.example.projetfrogger to javafx.fxml;
     exports com.example.projetfrogger;
+
+    exports graphicalElements;
+    opens graphicalElements to javafx.graphics;
 }
