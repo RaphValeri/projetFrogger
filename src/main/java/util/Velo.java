c948b2cdb1a7a110ac4efed92ca33cd248dade9f
rapha <raphael.valeri@ensta-bretagne.org>
2021-10-08 14:22:53 +0200

    ajout voiture et velo

---
+++ src/main/java/util/Velo.java
@@ -0,0 +1,7 @@
+package util;
+
+public interface Velo {
+    public int idVelo = 1;
+    public int tailleVelo = 1;
+    public void avancerVelo(int i);
+}
---
+++ src/main/java/util/Voiture.java
@@ -0,0 +1,8 @@
+package util;
+
+public interface Voiture {
+    public int idVoiture = 2;
+    public int tailleVoiture = 2;
+
+    public void avancerVoiture(int i);
+}
