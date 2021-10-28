bac6ed1e66aca5f242c2f1f8ddfdccc1420bbdc6
rapha <raphael.valeri@ensta-bretagne.org>
2021-10-28 15:32:20 +0200

    ajout fichier test AppTest

---
+++ src/test/java/graphicalElements/AppTest.java
@@ -0,0 +1,32 @@
+package graphicalElements;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AppTest {
+
+    @Test
+    void start() {
+    }
+
+    @Test
+    void imageFrog() {
+    }
+
+    @Test
+    void imageVehicule() {
+    }
+
+    @Test
+    void imageBackground() {
+    }
+
+    @Test
+    void rotate() {
+    }
+
+    @Test
+    void main() {
+    }
+}
